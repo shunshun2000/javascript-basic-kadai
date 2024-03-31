4f84d98057bbbfaf3c90e03e95053220ad6084a2
shunshun2000 <159131494+shunshun2000@users.noreply.github.com>
2024-03-31 15:14:33 +0900

    kadai_007

---
+++ kadai_007/calc.js
@@ -0,0 +1,23 @@
+let num = Math.floor(Math.random()*16)+ 1;
+
+console.log(num);
+
+/*3と5の倍数の場合*/
+if (num % 3===0 && num % 5 ===0) {
+    console.log(`3と5の倍数です`)
+}
+
+/*3の倍数の場合*/
+else if (num % 3===0) {
+    console.log(`3の倍数です`)
+}
+
+/*5の倍数の場合*/
+else if (num % 5===0) {
+    console.log(`5の倍数です`)
+}
+
+/*それ以外の場合*/
+else {
+    console.log(num);
+}
